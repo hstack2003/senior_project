29e66b37b5fac6dbe4d8ad39150789f4b5a0ecb6
hstack2003 <hannah.stackpole@gmail.com>
2026-05-07 09:52:07 -0700

    added county population data

---
+++ 05. population_dataset_creation.R
@@ -0,0 +1,25 @@
+library(readxl)
+library(tidyverse)
+library(tidyr)
+
+
+population_data_2000_2010 <- read_xlsx("Census_Data_Population/intercensal00s.xlsx",
+          skip = 2,
+          col_names = c("County",
+                        "2000_census",
+                        "2000",
+                        "2001",
+                        "2002",
+                        "2003",
+                        "2004",
+                        "2005",
+                        "2006",
+                        "2007",
+                        "2008",
+                        "2009",
+                        "2010_census",
+                        "2010")) |> 
+  filter(if_all(everything(), ~ !is.na(.))) |> 
+  pivot_longer(cols = `2000_census`:`2010`,
+               names_to = "year",
+               values_to = "population")
