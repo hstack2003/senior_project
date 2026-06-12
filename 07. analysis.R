e868ad8099fa0d82800b71c2e0f16eb2c9a02618
hstack2003 <hannah.stackpole@gmail.com>
2026-06-11 19:57:06 -0700

    updated analysis

--- 07. analysis.R
+++ 07. analysis.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(tidyr)
 library(fixest)
 library(gt)
+library(dplyr)
 source("06. tables_figures.R")
 
 #Main Results: Age group analysis by subject
@@ -16,7 +17,7 @@ subject_age_did_results <- istep_did_data |>
   nest(data = -c(subject, age_range)) |>
   mutate(mod = map(data, 
                    ~ feols(`Proficient %` ~ treated * post_treat | 
-                             IDOE_SCHOOL_ID + SCHOOL_YEAR_ID,
+                             IDOE_SCHOOL_ID + SCHOOL_YEAR_ID + GRADE_CODE,
                            cluster = ~ IDOE_SCHOOL_ID,
                            data = .x)),
          coefs = map(mod, broom::tidy)) |> 
@@ -61,7 +62,7 @@ subject_prof_did_results <- istep_did_data |>
   nest(data = -c(subject, prof_level, age_range)) |>
   mutate(mod = map(data, 
                    ~ feols(`Proficient %` ~ treated * post_treat | 
-                             IDOE_SCHOOL_ID + SCHOOL_YEAR_ID,
+                             IDOE_SCHOOL_ID + SCHOOL_YEAR_ID + GRADE_CODE,
                            cluster = ~ IDOE_SCHOOL_ID,
                            data = .x)),
          coefs = map(mod, broom::tidy)) |> 
@@ -92,31 +93,31 @@ prof_level_results_tbl <- subject_prof_did_results |>
   fmt_number(columns = contains("p.value"),decimals = 3) |>
   fmt_number(columns = contains("p_adj_holm"),decimals = 3)
 
-# Subject / Age / White Level results
-# subject_white_did_results <- istep_did_data |> 
+# # Subject / Age / White Level results
+# subject_white_did_results <- istep_did_data |>
 #   mutate(age_range = (case_when(GRADE_CODE %in% c("03", "04", "05") ~ "Elementary",
 #                                 GRADE_CODE %in% c("06", "07", "08") ~ "Middle",
 #                                 GRADE_CODE %in% c("09", "10") ~ "High School"))) |>
-#   bind_rows(istep_did_data |> mutate(age_range = "All Grades")) |> 
+#   bind_rows(istep_did_data |> mutate(age_range = "All Grades")) |>
 #   mutate(age_range = factor(age_range,
 #                             levels = c("All Grades", "Elementary", "Middle", "High School"))) |>
 #   filter(
-#     !is.na(white_level)) |> 
-#   nest(data = -c(subject, white_level, age_range)) |>
-#   mutate(mod = map(data, 
-#                    ~ feols(`Proficient %` ~ treated * post_treat | 
+#     !is.na(white_level_hi_lo)) |>
+#   nest(data = -c(subject, white_level_hi_lo, age_range)) |>
+#   mutate(mod = map(data,
+#                    ~ feols(`Proficient %` ~ treated * post_treat |
 #                              IDOE_SCHOOL_ID + SCHOOL_YEAR_ID,
 #                            cluster = ~ IDOE_SCHOOL_ID,
 #                            data = .x)),
-#          coefs = map(mod, broom::tidy)) |> 
-#   select(subject, white_level, age_range, coefs) |> 
-#   unnest(cols = coefs) |> 
-#   group_by(subject) |> 
-#   mutate(p_adj_holm = p.adjust(p.value, method = "holm")) |> 
-#   ungroup() |> 
-#   arrange(age_range, white_level)
+#          coefs = map(mod, broom::tidy)) |>
+#   select(subject, white_level_hi_lo, age_range, coefs) |>
+#   unnest(cols = coefs) |>
+#   group_by(subject) |>
+#   mutate(p_adj_holm = p.adjust(p.value, method = "holm")) |>
+#   ungroup() |>
+#   arrange(age_range, white_level_hi_lo)
 # 
-# white_level_results_tbl <- subject_white_did_results |> 
+# white_level_results_tbl <- subject_white_did_results |>
 #   mutate(estimate_se = sprintf("%.3f\n(%.2f)", estimate, std.error)) |>
 #   select(age_range, subject, white_level, estimate_se, p.value, p_adj_holm) |>
 #   pivot_wider(names_from = subject,
@@ -135,33 +136,33 @@ prof_level_results_tbl <- subject_prof_did_results |>
 #               columns = c(estimate_se_Math, p.value_Math, p_adj_holm_Math)) |>
 #   fmt_number(columns = contains("p.value"),decimals = 3) |>
 #   fmt_number(columns = contains("p_adj_holm"),decimals = 3)
-
-# FRL / Age / Subject 
-# subject_free_meal_did_results <- istep_did_data |> 
+# 
+# # FRL / Age / Subject 
+# subject_free_meal_did_results <- istep_did_data |>
 #   mutate(age_range = (case_when(GRADE_CODE %in% c("03", "04", "05") ~ "Elementary",
 #                                 GRADE_CODE %in% c("06", "07", "08") ~ "Middle",
 #                                 GRADE_CODE %in% c("09", "10") ~ "High School"))) |>
-#   bind_rows(istep_did_data |> mutate(age_range = "All Grades")) |> 
+#   bind_rows(istep_did_data |> mutate(age_range = "All Grades")) |>
 #   mutate(age_range = factor(age_range,
 #                             levels = c("All Grades", "Elementary", "Middle", "High School"))) |>
 #   filter(
-#     !is.na(free_meal_level)) |> 
+#     !is.na(free_meal_level)) |>
 #   nest(data = -c(subject, free_meal_level, age_range)) |>
-#   mutate(mod = map(data, 
-#                    ~ feols(`Proficient %` ~ treated * post_treat | 
+#   mutate(mod = map(data,
+#                    ~ feols(`Proficient %` ~ treated * post_treat |
 #                              IDOE_SCHOOL_ID + SCHOOL_YEAR_ID,
 #                            cluster = ~ IDOE_SCHOOL_ID,
 #                            data = .x)),
-#          coefs = map(mod, broom::tidy)) |> 
-#   select(subject, free_meal_level, age_range, coefs) |> 
-#   unnest(cols = coefs) |> 
-#   filter(term == "treated:post_treat") |> 
-#   group_by(subject) |> 
-#   mutate(p_adj_holm = p.adjust(p.value, method = "holm")) |> 
-#   ungroup() |> 
+#          coefs = map(mod, broom::tidy)) |>
+#   select(subject, free_meal_level, age_range, coefs) |>
+#   unnest(cols = coefs) |>
+#   filter(term == "treated:post_treat") |>
+#   group_by(subject) |>
+#   mutate(p_adj_holm = p.adjust(p.value, method = "holm")) |>
+#   ungroup() |>
 #   arrange(age_range, free_meal_level)
 # 
-# free_meal_level_results_tbl <- subject_free_meal_did_results |> 
+# free_meal_level_results_tbl <- subject_free_meal_did_results |>
 #   mutate(estimate_se = sprintf("%.3f\n(%.2f)", estimate, std.error)) |>
 #   select(age_range, subject, free_meal_level, estimate_se, p.value, p_adj_holm) |>
 #   pivot_wider(names_from = subject,
